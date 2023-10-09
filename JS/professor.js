e9069b5a8c98dae8263534fb32c29a0691c695b3
Eder Santos <edersf1999@gmail.com>
2023-10-09 18:50:22 -0300

    first commit

---
+++ JS/professor.js
@@ -0,0 +1,29 @@
+
+const mostrarProfessores = (professores) => {
+    const conteudo = document.getElementById('tabela')
+    professores.forEach((professor) => {
+        const tabelaHTML = `
+            <div class="tbody" id="tbody">
+                <span class="nomeTbody">${professor.nome}</span>
+                <span class="disciplinaTbody">${professor.disciplina}</span>
+                <span class="perfilTabelaTbody">${professor.perfil}</span>
+                <span class="ativoTbody">on/off</span>
+                <span class="acoesTbody">Ações</span> 
+            </div>
+        `
+        conteudo.innerHTML = conteudo.innerHTML + tabelaHTML
+    })
+}
+
+const getProfessores = async () => {
+    const apiURL = await fetch('http:///localhost:3000/professor')
+    const professores = await apiURL.json()
+    mostrarProfessores(professores)
+    console.log(professores)
+}
+
+getProfessores()
+
+const novoUsuario = () => {
+    window.location = "../../../pages/novoUsuario/index.html"
+}
--- JS/usuario.js
+++ JS/usuario.js
@@ -1,1 +1,26 @@
+let exibirSubMenu = false
 
+function mostrarMenu(){
+    const imgSeta = document.querySelector("#setaUsers")
+    const subMenu = document.querySelector("#subMenu")
+    const menuUser = document.querySelector("#menuUser")
+    const span = document.querySelector("#span")
+
+    if(!exibirSubMenu){
+        imgSeta.src="../../assets/images/setaParaBaixo.svg"
+        subMenu.style.display = "block"
+        menuUser.style.background = "#faa81a7b"
+        span.style.color = "#FFFF"
+        exibirSubMenu = true
+    }else{
+        imgSeta.src="../../assets/images/setaUsersMarrom.svg"
+        subMenu.style.display = "none"
+        menuUser.style.background = "none"
+        span.style.color = "#AA6F34"
+        exibirSubMenu = false
+
+    }
+
+    
+    
+}
